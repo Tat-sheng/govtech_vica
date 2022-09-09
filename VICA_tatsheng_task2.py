252f2a2c5bd95a6a5e27281774423fcfc61c9181
Tats Hi <tat97g@gmail.com>
2022-09-10 01:17:40 +0800

    Completed Task 2

--- VICA_tatsheng_task2.py
+++ VICA_tatsheng_task2.py
@@ -5,8 +5,18 @@ warnings.filterwarnings('ignore')
 import json
 import pandas as pd
 import numpy as np
+from pymongo import MongoClient
 
+# Instructions
+#  - You can run the code to print to review the structured format
+#  - You can uncomment the highlighted chunks and insert your URL to import the data into your MongoDB
 
+# Uncomment below ===========================================================
+# CONNECTION_URL = "mongodb://root:rootpassword@localhost:27017"
+# db = MongoClient(host = CONNECTION_URL)
+# ===========================================================================
+
+# Load and store 
 df = pd.read_csv(r'insurance_data.csv', sep = ';')
 headers = df.columns.tolist()
 
@@ -21,7 +31,7 @@ df = df.replace('Yes', True).replace('Y', True).replace('No', False).replace('N'
 df.healthInsurance = df.healthInsurance.apply(lambda x: True if x != False else False)
 df.is45OrOlder = df.is45OrOlder.apply(lambda x: True if x == 1 else False)
 
-# L Dataframe, W array
+# Convert to array for easier manipulation
 df_array = df.to_numpy()
 
 # Nan is no longer my friend, list is now my best friend
@@ -31,13 +41,13 @@ def list_rejector(x):
     else:
         return x.split(',')
 
-# Sorry
+# Structure function
 def one_liner(line, headers): 
     out_dict = {}
     tl = {'hasPolicy': line[6], 'hasMultiplePolicies': line[7]}
     hi = {'hasPolicy': line[8],'riders': list_rejector(line[9])}
 
-    for i in range(15):
+    for i in range(15): # 15 is the length of headers list 
         if i == 6:
             out_dict['termLifeInsurance'] = tl
         elif i == 7:
@@ -50,5 +60,13 @@ def one_liner(line, headers):
             out_dict[headers[i]] = line[i]
     return out_dict
 
+
 for row in df_array:
     print(one_liner(row, headers))
+    # Uncomment below =======================================================
+    # db.segment.insert(json.dumps(one_liner(row, headers)))
+    # =======================================================================
+
+# The structured code is original but for the DB insertions (Line 15,16 & 67), I took reference to 
+# https://webdamn.com/import-csv-file-into-mongodb-using-python/
+#                                                                                      - Tat Sheng
